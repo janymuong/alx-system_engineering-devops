c4abf92378e69ae9e64f504508c7b0f6be750d78
Jany Muong <me.roumuong@gmail.com>
2023-06-23 15:07:27 +0300

    puppet: create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp : with permissions, owner, group, and content
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
