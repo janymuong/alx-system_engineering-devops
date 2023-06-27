08569529a0a7ce998780351746ee70cbca7151f2
Jany Muong <me.roumuong@gmail.com>
2023-06-28 01:30:37 +0300

    puppet script: install nginx, configure nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,21 @@
-# puppet: install and configure Ngin
+# puppet: install and configure Nginx
 
-class { 'nginx':
-  ensure  => installed,
-  require => Package['nginx'],
+package { 'nginx':
+  ensure => installed,
 }
 
-file_line { 'Add redirection, 301':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://stackoverflow.com/ permanent;',
-  require => Class['nginx'],
-  notify  => Service['nginx'],
+file_line { 'server_config':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://stackoverflow.com/ permanent;',
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
 }
 
 service { 'nginx':
   ensure  => running,
-  enable  => true,
   require => Package['nginx'],
 }
