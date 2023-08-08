1a03e8c118e6b111120792e8a7d27ebb004eac65
Jany Muong <me.roumuong@gmail.com>
2023-08-08 15:16:43 +0300

    parse, count, sort API data from recursive API calls

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''
+module 100-count:
+Queries the Reddit API recursively, parses the title of all hot articles
+and prints a sorted count of given keywords
+in the titles of all hot articles for a subreddit
+'''
+import requests
+
+
+def count_words(subreddit, word_list, after=None, word_count={}):
+    '''
+    prints a sorted count of given keywords in the titles of all hot articles.
+    '''
+    sub_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    user_agent = {'User-Agent': 'MyRedditScript/1.0'}
+    params = {'after': after} if after else {}
+
+    res = requests.get(sub_url, headers=user_agent, params=params,
+                       allow_redirects=False)
+
+    if res.status_code == 200:
+        try:
+            data = res.json()
+            posts = data['data']['children']
+
+            if not posts:
+                sorted_words = sorted(word_count.items(),
+                                      key=lambda x: (-x[1], x[0]))
+                for word, count in sorted_words:
+                    print(f'{word}: {count}')
+                return
+
+            for post in posts:
+                title = post['data']['title'].lower()
+                for word in word_list:
+                    keyword = word.lower()
+                    if f' {keyword} ' in f' {title} ':
+                        if keyword in word_count:
+                            word_count[keyword] += 1
+                        else:
+                            word_count[keyword] = 1
+
+            after = data['data']['after']
+            return count_words(subreddit, word_list, after, word_count)
+        except (KeyError, ValueError):
+            return
+    else:
+        return
