318eef2bb335a2e3c78428cbc010721193f11a86
Jany Muong <me.roumuong@gmail.com>
2023-08-17 10:59:29 +0300

    fix open file limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# manifest file: 0-the_sky_is_the_limit_solved.pp
+# increase the open file limit for Nginx using the exec resource:
+
+exec { 'fix--for-nginx':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
